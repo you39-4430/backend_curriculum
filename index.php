bf5b6d9e981ad184e0d532b42c7f564e3a44c2fb
吉澤勇作 <>
2022-02-14 14:59:30 +0900

    削除機能

--- index.php
+++ index.php
@@ -12,6 +12,15 @@ try {
 
 $users = $db->query("SELECT * FROM users WHERE del_flg = false")
     ->fetchAll(PDO::FETCH_ASSOC);
+
+if (!empty($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "UPDATE users SET del_flg = true WHERE id = :id AND del_flg = false";
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':id', $id, PDO::PARAM_STR);
+    $stmt->execute();
+    header('location: http://localhost:8080');
+}
 ?>
 <html lang="ja">
 
@@ -44,16 +53,23 @@ $users = $db->query("SELECT * FROM users WHERE del_flg = false")
                 </thead>
                 <tbody>
                     <?php foreach ($users as $user) : ?>
-                        <tr>
-                            <td class="border px-4 py-2"><?php echo $user['name']; ?></td>
-                            <td class="border px-4 py-2"><?php echo $user['address']; ?></td>
-                            <td class="border px-4 py-2"><?php echo $user['tel']; ?></td>
-                            <td class="border px-4 py-2">
-                                <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                                    <a href="<?php echo '/edit.php?id=' . $user['id'] ?>">編集</a>
-                                </button>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td class="border px-4 py-2"><?php echo $user['name']; ?></td>
+                        <td class="border px-4 py-2"><?php echo $user['address']; ?></td>
+                        <td class="border px-4 py-2"><?php echo $user['tel']; ?></td>
+                        <td class="border px-4 py-2">
+                            <button
+                                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                <a href="<?php echo '/edit.php?id=' . $user['id'] ?>">編集</a>
+                            </button>
+                        </td>
+                        <td class="border px-4 py-2">
+                            <button
+                                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                <a href="<?php echo '?id=' . $user['id'] ?>">削除</a>
+                            </button>
+                        </td>
+                    </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
